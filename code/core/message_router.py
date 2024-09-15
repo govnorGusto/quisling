c715ecde3cde553925e0f5aa77c2342b539956fe
Gustav Englud <gustav.englund@rodengames.com>
2024-09-15 21:36:50 +0200

    Removed Logs

--- code/core/message_router.py
+++ code/core/message_router.py
@@ -7,17 +7,14 @@ class Message_Router:
     def register_callback(self, event, callback):
         if event in self._callbacks:
             self._callbacks[event].append(callback)
-            print("Added callback, to existing Event: " + str(callback))
             return
 
         self._callbacks[event] = [callback]
-        print("Added callback, to new Event: " + str(callback))
 
     def clear_callback(self, callback):
         for key in self._callbacks:
             if callback in self._callbacks[key]:
                 self._callbacks[key].remove(callback)
-                print("removed callback: " + str(callback))
 
     def broadcast_message(self, event, eventdata=0):
         if event not in self._callbacks:
@@ -27,5 +24,3 @@ class Message_Router:
         if callbacks:
             for callback in callbacks:
                 callback(eventdata)
-        else:
-            print("EmptyCallbackList")
