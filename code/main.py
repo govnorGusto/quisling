4cf9b0bb8a3c97c16b2f5820487f1396e206a52e
Gustav Englud <gustav.englund@rodengames.com>
2024-09-15 18:38:33 +0200

    Game loop housekeeping

--- code/main.py
+++ code/main.py
@@ -1,9 +1,11 @@
+from logging import shutdown
 from settings import *
 from level.level import Level
 from turn_manager import Turn_Manager
 from button import Button
 from core.message_router import Message_Router
 from core.input_manager import Input_Manager
+
 
 class Game:
     def __init__(self):
@@ -13,48 +15,50 @@ class Game:
         pygame.display.set_caption("the quisling project")
         self.current_stage = Level()
         self.running = True
-        
+
         self.message_router = Message_Router()
         self.input_manager = Input_Manager(self)
         self.message_router.register_callback(pygame.QUIT, self.OnQuit)
         self.message_router.register_callback(pygame.KEYDOWN, self.OnKeyDown)
 
         self.turn_manager = Turn_Manager()
-        self.button = Button(WINDOW_WIDTH - 100,
-                             WINDOW_HEIGHT - 70, "NEXT")
+        self.button = Button(WINDOW_WIDTH - 100, WINDOW_HEIGHT - 70, "NEXT")
 
     def process_input(self):
         self.input_manager.process_input()
-        
+
     def OnQuit(self, eventdata):
         self.running = False
-    
+
     ### TODO: This binding should happen inside the turn_manager, game does arguably not give a shit
-    ###       leaving as is for now to avoid conflicts in turn_manager.py // Herjeman (GUSTAV) 
+    ###       leaving as is for now to avoid conflicts in turn_manager.py // Herjeman (GUSTAV)
     def OnKeyDown(self, eventdata):
         self.turn_manager.move(eventdata.key)
 
-    def update(self, dt):
+    def update(self, delta_time):
         self.turn_manager.update()
 
-    def draw(self, dt):
+    def draw(self, delta_time):
         self.current_stage.draw()
         self.turn_manager.players.draw(self.display_surface)
         if self.button.draw():
             self.turn_manager.change_player()
 
-    def run(self):
-        while self.running:
-            dt = self.clock.tick(FPS) / 1000
+        pygame.display.update()
 
-            self.process_input()
-            self.update(dt)
-            self.draw(dt)
-
-            pygame.display.update()
-
+    def shutdown():
         pygame.quit()
         sys.exit()
+
+    def run(self):
+        while self.running:
+            delta_time = self.clock.tick(FPS) / 1000
+
+            self.process_input()
+            self.update(delta_time)
+            self.draw(delta_time)
+
+        self.shutdown()
 
 
 if __name__ == "__main__":
