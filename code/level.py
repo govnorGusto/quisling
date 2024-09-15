335af5bd283e865c89d4be1665a6083ee3e8a3f6
gustav <gustav.hardselius@gmail.com>
2024-09-15 07:26:30 +0200

    ADD initial game loop and file-structure

---
+++ code/level.py
@@ -0,0 +1,9 @@
+from settings import *
+
+
+class Level:
+    def __init__(self):
+        self.display_surface = pygame.display.get_surface()
+
+    def run(self, dt):
+        self.display_surface.fill("purple")
---
+++ code/settings.py
@@ -0,0 +1,7 @@
+import pygame, sys
+from pygame.math import Vector2 as vector
+from os.path import join
+
+# this is where we store the settings
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
