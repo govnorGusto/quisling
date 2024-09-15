2be28f9cb101d06e6142ab0051c35fa93a137d2e
Gustav Englud <gustav.englund@rodengames.com>
2024-09-15 19:50:27 +0200

    add missing self parameter

--- code/main.py
+++ code/main.py
@@ -31,7 +31,7 @@ class Game:
         self.running = False
 
     ### TODO: This binding should happen inside the turn_manager, game does arguably not give a shit
-    ###       leaving as is for now to avoid conflicts in turn_manager.py // Herjeman (GUSTAV)
+    ###       Leaving as is for now to avoid conflicts in turn_manager.py // Herjeman (GUSTAV)
     def OnKeyDown(self, eventdata):
         self.turn_manager.move(eventdata.key)
 
@@ -46,7 +46,7 @@ class Game:
 
         pygame.display.update()
 
-    def shutdown():
+    def shutdown(self):
         pygame.quit()
         sys.exit()
 
