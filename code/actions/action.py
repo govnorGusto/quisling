5d498b4873517c7f33803315634299ced79e4022
gustav <gustav.hardselius@gmail.com>
2024-09-20 15:26:01 +0200

    More intuitive movement

--- code/actions/action.py
+++ code/actions/action.py
@@ -1,11 +1,17 @@
-
 class Action:
     def __init__(self, owner, action_cost: int):
 
         self.owner = owner
         self.action_cost = action_cost
-        self.directions = {"up": (0, -1), "down": (0, 1),
-                           "left": (-1, 0), "right": (1, 0)}
+        # self.directions = {"up": (0, -1), "down": (0, 1),
+        #                    "left": (-1, 0), "right": (1, 0)}
+        # This feels like a more natural move pattern
+        self.directions = {
+            "up": (1, 0),
+            "down": (0, 1),
+            "left": (0, -1),
+            "right": (0, -1),
+        }
 
     def can_execute(self):
         pass
@@ -26,10 +32,9 @@ class Action:
                 return self.directions["down"]
             case 3:
                 return self.directions["left"]
-    
+
     def take_damage(self, obj, damage):
         if self.owner.game.turn_manager.resolve:
             obj.health -= damage
         else:
             print(f"{obj} has taken {damage} damage")
-
