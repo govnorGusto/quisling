e18ba05b82e1a74fc756618643ed570c592494e2
Niklas <niklasjokiaho@live.com>
2024-09-20 19:02:21 +0900

    added can_execute and in bound check on attack actions

--- code/actions.py
+++ code/actions.py
@@ -1,12 +1,14 @@
 from core.sprite_object import Sprite_object
 
+
 class Action:
-    def __init__(self, owner, action_cost:int):
+    def __init__(self, owner, action_cost: int):
 
         self.owner = owner
         self.action_cost = action_cost
-        self.directions = {"up": (0, -1), "down": (0, 1), "left": (-1, 0), "right": (1, 0)}
-    
+        self.directions = {"up": (0, -1), "down": (0, 1),
+                           "left": (-1, 0), "right": (1, 0)}
+
     def can_execute(self):
         pass
 
@@ -51,10 +53,11 @@ class Move(Action):
             self.owner.x = new_x
             self.owner.y = new_y
             self.owner.game.grid.move(self.owner, self.owner.x, self.owner.y)
-            self.owner.rect.topleft = self.owner.game.grid.grid_to_screen(self.owner.x, self.owner.y)
+            self.owner.rect.topleft = self.owner.game.grid.grid_to_screen(
+                self.owner.x, self.owner.y)
             if hasattr(self.owner, "stamina"):
                 self.owner.stamina -= self.action_cost
-                
+
                 # Set the direction of the character
                 if dx > 0:
                     self.owner.facing = 1
@@ -75,10 +78,10 @@ class Move(Action):
             else:
                 results[direction] = False
         return results
-    
+
     def __repr__(self):
         return "Move_action"
-    
+
 
 class Bash_Attack(Action):
     def __init__(self, owner, action_cost=1):
@@ -89,14 +92,17 @@ class Bash_Attack(Action):
             print("Not enough stamina")
             return False
         return True
-    
+
     def execute(self):
+        self.can_execute()
         facing = self.get_direction(self.owner.facing)
-        attack = Sprite_object(self.owner.x + facing[0], self.owner.y + facing[1])
+        if self.owner.game.grid.in_bounds(self.owner.x + facing[0], self.owner.y + facing[1]):
+            Sprite_object(self.owner.x + facing[0], self.owner.y + facing[1])
         self.owner.stamina -= self.action_cost
-    
+
     def __repr__(self):
         return "bash_attack"
+
 
 class Spinning_Attack(Action):
     def __init__(self, owner, action_cost=1):
@@ -107,12 +113,13 @@ class Spinning_Attack(Action):
             print("Not enough stamina")
             return False
         return True
-    
+
     def execute(self):
+        self.can_execute()
         val = [-1, 0, 1]
         for x in val:
             for y in val:
-                if x == 0 and y == 0:
+                if x == 0 and y == 0 or not self.owner.game.grid.in_bounds(self.owner.x + x, self.owner.y + y):
                     continue
                 else:
                     Sprite_object(self.owner.x + x, self.owner.y + y)
@@ -120,4 +127,3 @@ class Spinning_Attack(Action):
 
     def __repr__(self):
         return "spinning_attack"
-
